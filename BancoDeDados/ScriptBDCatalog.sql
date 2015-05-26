649b9c0d02a55babf665370cc1296fd14977a6a9
jonnathanbruno <jonnathanbruno@gmail.com>
2015-05-26 15:10:39 -0300

    [0PcEPjDG Ajuste] - Ajuste no banco de dados

--- BancoDeDados/ScriptBDCatalog.sql
+++ BancoDeDados/ScriptBDCatalog.sql
@@ -2,64 +2,66 @@ CREATE DATABASE Catalog;
 USE Catalog;
 
 CREATE TABLE tb_Usuario(
-	id_usuario int primary key identity(1,1),
+	id int primary key identity(1,1),
 	email varchar(50) not null,
 	senha varchar(50) not null,
 	nome varchar(100) not null
 );
 
 CREATE TABLE tb_SenhaAlternativa(
-	id_senhaAlternativa int primary key identity(1,1),
+	idUsuario int,
 	senha varchar(50),
 	dataDeCriacao Date,
-	id_usuario_fk int foreign key references tb_Usuario(id_usuario)
+	PRIMARY KEY CLUSTERED(idUsuario),
+	FOREIGN KEY (idUsuario) REFERENCES tb_Usuario(id)
 );
 
 CREATE TABLE tb_Chave(
-	id_chave int primary key identity(1,1),
+	idUsuario int,
 	token varchar(50),
 	ultimoAcesso time,
-	id_usuario_fk int foreign key references tb_Usuario(id_usuario)
+	PRIMARY KEY CLUSTERED(idUsuario),
+	FOREIGN KEY (idUsuario) REFERENCES tb_Usuario(id)
 );
 
 CREATE TABLE tb_Lista(
-	id_lista int primary key identity(1,1),
+	id int primary key identity(1,1),
 	titulo varchar(50),
-	id_usuario_fk int foreign key references tb_Usuario(id_usuario)
+	idUsuario int foreign key references tb_Usuario(id)
 )
 
 CREATE TABLE tb_Fabricante(
-	id_fabricante int primary key identity(1,1),
+	id int primary key identity(1,1),
 	fabricante varchar(50)
 )
 
 CREATE TABLE tb_Tipo(
-	id_tipo int primary key identity(1,1),
+	id int primary key identity(1,1),
 	tipo varchar(50)
 )
 
 CREATE TABLE tb_Produto(
-	id_produto int primary key identity(1,1),
+	id int primary key identity(1,1),
 	nome varchar(100),
 	codigoDeBarras varchar(50),
-	id_fabricante_fk int foreign key references tb_Fabricante(id_fabricante),
-	id_tipo_fk int foreign key references tb_tipo(id_tipo)
+	idFabricante int foreign key references tb_Fabricante(id),
+	idTipo int foreign key references tb_tipo(id)
 );
 
 CREATE TABLE tb_ProdutoDaLista(
-	id_produtoDaLista int primary key identity(1,1),
+	id int primary key identity(1,1),
 	quantidade int,
-	id_produto_fk int foreign key references tb_Produto(id_produto),
-	id_lista_fk int foreign key references tb_Lista(id_lista)
+	idProduto int foreign key references tb_Produto(id),
+	idLista int foreign key references tb_Lista(id)
 )
 
 CREATE TABLE tb_Estabelecimento(
-	id_estabelecimento int primary key identity(1,1),
+	id int primary key identity(1,1),
 	estabelecimento varchar(50)
 )
 
 CREATE TABLE tb_EnderecoEstabelecimento(
-	id_enderecoEstabelecimento int primary key identity(1,1),
+	id int primary key identity(1,1),
 	rua varchar(100),
 	cidade varchar(100),
 	estado varchar(100),
@@ -67,14 +69,14 @@ CREATE TABLE tb_EnderecoEstabelecimento(
 	cep varchar(100),
 	latitude DOUBLE PRECISION,
 	longitude DOUBLE PRECISION,
-	id_estabelecimento_fk int foreign key references tb_Estabelecimento(id_estabelecimento)
+	idEstabelecimento int foreign key references tb_Estabelecimento(id)
 )
 
 CREATE TABLE tb_Item(
-	id_iten int primary key identity(1,1),
+	id int primary key identity(1,1),
 	preco DOUBLE PRECISION,
 	compraRecente date,
 	qualificacao int,
-	id_estabelecimento_fk int foreign key references tb_Estabelecimento(id_estabelecimento),
-	id_produto_fk int foreign key references tb_Produto(id_produto),
+	idEstabelecimento int foreign key references tb_Estabelecimento(id),
+	idProduto int foreign key references tb_Produto(id)
 );
