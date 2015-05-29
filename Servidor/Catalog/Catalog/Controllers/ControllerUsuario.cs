d3f8f49d7002d96c1fa8a9533fa87c785331994c
JonnathanBruno <jonnathanbruno@gmail.com>
2015-05-29 08:35:03 -0300

    [LcbOdoPh]  - CDU-EfetuarCadastro 1.0

--- Servidor/Catalog/Catalog/Controllers/ControllerUsuario.cs
+++ Servidor/Catalog/Catalog/Controllers/ControllerUsuario.cs
@@ -4,6 +4,7 @@ using Catalog.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Script.Serialization;
 
@@ -11,14 +12,40 @@ namespace Catalog.Controllers
 {
 	public class ControllerUsuario : IControllerUsuario
 	{
+        //_____________ CADASTRAR USUARIO _____________//
 		public string realizarCadastro(string dtoUsuario)
 		{
 			JavaScriptSerializer js = new JavaScriptSerializer();
+            DtoUsuario usuario = js.Deserialize<DtoUsuario>(dtoUsuario);
+            Usuario mUsuario = new Usuario();
 			DtoRetorno retorno;
+            Regex rg = new Regex(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$");
 
-			return "";
+            //---validando nome, email, senha
+            if(
+              (usuario.nome.Length >= 3) &&
+              (rg.IsMatch(usuario.email)) &&  
+              (usuario.senha.Length >3)  )
+            {
+                try
+                {
+                    DtoChave chave = mUsuario.cadastrarUsuario(usuario.email, usuario.senha, usuario.nome);
+                    retorno = new DtoRetornoObjeto(chave);
+                }
+                catch
+                {
+                    retorno = new DtoRetornoErro("E-mail já cadastrado!");
+                }
+            }
+            else
+            {
+                return js.Serialize(new DtoRetornoErro("Existem campos inválidos!"));
+            }
+
+            return js.Serialize(retorno);
 		}
 
+        //______________ ALTERAR SENHA ________________//
 		public string alterarSenha(string dtoChave, string dtoUsuario)
 		{
 			JavaScriptSerializer js = new JavaScriptSerializer();
