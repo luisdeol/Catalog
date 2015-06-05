1b59053b63f675caa9714a35e3e05834d6f26ed0
Marcos <marcos.antas@hotmail.com.br>
2015-06-05 19:34:51 -0300

    Metodos de Teste (Lista)
    Metodos de teste para ControllerLista criados

---
+++ Servidor/Catalog/Catalog/Controllers/ControllerLista.cs
@@ -0,0 +1,58 @@
+﻿using Catalog.Controllers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace Catalog.Controllers
+{
+	public class ControllerLista : IControllerLista
+	{
+		public string criarLista(string dtoChave, string dtoLista)
+		{
+			JavaScriptSerializer js = new JavaScriptSerializer();
+			return "";
+		}
+
+		public string abrirLista(string dtoChave, string dtoLista)
+		{
+			return "";
+		}
+
+		public string editarLista(string dtoChave, string dtoLista)
+		{
+			return "";
+		}
+
+		public string excluirLista(string dtoChave, string dtoLista)
+		{
+			return "";
+		}
+
+		public string pesquisarLista(string dtoChave, string parametros)
+		{
+			return "";
+		}
+
+		public string listarProdutos(string dtoChave, string dtoLista)
+		{
+			return "";
+		}
+
+		public string listarItensEm(string dtoChave, string dtoLista, string dtoEstabelecimento)
+		{
+			return "";
+		}
+
+		public string adicionarProduto(string dtoChave, string dtoLista, string dtoProduto)
+		{
+			return "";
+		}
+
+		public string removerProduto(string dtoChave, string dtoLista, string dtoProduto)
+		{
+			return "";
+		}
+	}
+}
---
+++ Servidor/Catalog/Catalog/Webservices/WsLista.asmx.cs
@@ -0,0 +1,83 @@
+﻿using Catalog.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Catalog.Webservices
+{
+	/// <summary>
+	/// Summary description for WebService2
+	/// </summary>
+	[WebService(Namespace = "http://tempuri.org/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	[System.ComponentModel.ToolboxItem(false)]
+	// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+	[System.Web.Script.Services.ScriptService]
+	public class WsLista : System.Web.Services.WebService
+	{
+		[WebMethod]
+		public string criarLista(string dtoChave, string dtoLista)
+		{
+			ControllerLista cLista = new ControllerLista();
+			return cLista.criarLista(dtoChave, dtoLista);
+		}
+
+		[WebMethod]
+		public string abrirLista(string dtoChave, string dtoLista)
+		{
+			ControllerLista cLista = new ControllerLista();
+			return cLista.abrirLista(dtoChave, dtoLista);
+		}
+
+		[WebMethod]
+		public string editarLista(string dtoChave, string dtoLista)
+		{
+			ControllerLista cLista = new ControllerLista();
+			return cLista.editarLista(dtoChave, dtoLista);
+		}
+
+		[WebMethod]
+		public string excluirLista(string dtoChave, string dtoLista)
+		{
+			ControllerLista cLista = new ControllerLista();
+			return cLista.excluirLista(dtoChave, dtoLista);
+		}
+
+		[WebMethod]
+		public string pesquisarLista(string dtoChave, string parametros)
+		{
+			ControllerLista cLista = new ControllerLista();
+			return cLista.pesquisarLista(dtoChave, parametros);
+		}
+
+		[WebMethod]
+		public string listarProdutos(string dtoChave, string dtoLista)
+		{
+			ControllerLista cLista = new ControllerLista();
+			return cLista.listarProdutos(dtoChave, dtoLista);
+		}
+
+		[WebMethod]
+		public string listarItensEm(string dtoChave, string dtoLista, string dtoEstabelecimento)
+		{
+			ControllerLista cLista = new ControllerLista();
+			return cLista.listarItensEm(dtoChave, dtoLista, dtoEstabelecimento);
+		}
+
+		[WebMethod]
+		public string adicionarProduto(string dtoChave, string dtoLista, string dtoProduto)
+		{
+			ControllerLista cLista = new ControllerLista();
+			return cLista.adicionarProduto(dtoChave, dtoLista, dtoProduto);
+		}
+
+		[WebMethod]
+		public string removerProduto(string dtoChave, string dtoLista, string dtoProduto)
+		{
+			ControllerLista cLista = new ControllerLista();
+			return cLista.removerProduto(dtoChave, dtoLista, dtoProduto);
+		}
+	}
+}
