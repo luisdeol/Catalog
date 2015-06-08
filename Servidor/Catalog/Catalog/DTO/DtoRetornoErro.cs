a7e68d2cdda7858e0c1b6fa6bdcadec71add8013
Marcos <marcos.antas@hotmail.com.br>
2015-06-08 13:28:12 -0300

    Enumeração dos erros iniciada

--- Servidor/Catalog/Catalog/DTO/DtoRetornoErro.cs
+++ Servidor/Catalog/Catalog/DTO/DtoRetornoErro.cs
@@ -16,5 +16,26 @@ namespace Catalog.DTO
 			this.mensagem = mensagem;
 			this.codigoErro = codigoErro;
 		}
+
+		public DtoRetornoErro(Enum tipo, string especificacao, string destino = "this")
+			: base("NAK", destino)
+		{
+			codigoErro = tipo.ToString();
+			switch(tipo)
+			{
+				case Enum.ErroCriacao:
+					mensagem = "Não foi possível realizar o cadastro de '" + especificacao + "'";
+					break;
+				case Enum.ObjetoNaoEncontrado:
+					mensagem = "Nenhum resultado foi encontrado";
+					break;
+				case Enum.CampoInvalido:
+					mensagem = "Existem campos com valores inválidos:\n" + especificacao;
+					break;
+				case Enum.ChaveInvalida:
+					mensagem = "É nescessário realizar login para utilizar o aplicativo!";
+					break;
+			}
+		}
 	}
 }
---
+++ Servidor/Catalog/Catalog/DTO/Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Catalog.DTO
+{
+	public enum Enum
+	{
+		ChaveInvalida = 100,
+		ErroCriacao = 102,
+		ObjetoNaoEncontrado = 103,
+		CampoInvalido = 104
+	}
+}
