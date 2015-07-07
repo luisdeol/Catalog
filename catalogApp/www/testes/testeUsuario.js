c7a66227c3d888fe3ca2ec6d6ffafcfa431395dc
Jonnathan Bruno <20131014040145@cnat266565.ifrn.local>
2015-07-07 08:27:47 -0300

    TestesUsuario

--- catalogApp/www/testes/testeUsuario.js
+++ catalogApp/www/testes/testeUsuario.js
@@ -1,5 +1,5 @@
 describe("Testes de Usuario", function () {
-    beforeEach(module("catalogApp",['catalogApp.controllers']));
+    beforeEach(module("UsuarioControllers"));
 
     describe("UsuarioController", function () {
         var scope,
@@ -12,14 +12,35 @@ describe("Testes de Usuario", function () {
 
         it("(Cadastro)Senhas diferentes devem retornar falso", function () {
             controller("UsuarioController", {$scope: scope});
-			var retorno = scope.cadastrar({nome: "jonnathan", email: "jonnathan@gmail.com",senha: "1234", confirmarSenha: "12345"});
+			var retorno = scope.cadastrar({nome: "jonnathan", email: "jonnathan@gmail.com",senha: "123456", confirmarSenha: "1234567"});
             expect(retorno).toBe(false);
         });
 		
-		it("(Cadastro)Preencha todos os campos", function () {
+		it("(Cadastro)Senha deve conter mais de 5 digitos", function () {
             controller("UsuarioController", {$scope: scope});
-			var retorno = scope.cadastrar();
+			
+			var user = new Object();
+			user.nome = "jonnathan";
+			user.email = "jonnathan@gmail.com";
+			user.senha = "1234";
+			user.confirmarSenha = "123456";
+			
+			var retorno = scope.cadastrar(user);
             expect(retorno).toBe(false);
+        });
+		
+		it("(Cadastro)Email inválido", function () {
+            controller("UsuarioController", {$scope: scope});
+			
+			var user = new Object();
+			user.nome = "jonnathan";
+			user.email = "jonnathangmail.com"; 
+			user.senha = "123456";
+			user.confirmarSenha = "123456";
+			
+			scope.cadastrar(user);
+			
+            expect(scope.erro).toBe(true);
         });
 		
 		it("(Login)Preencha todos os campos", function () {
